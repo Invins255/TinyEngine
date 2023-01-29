c63dd4cd332aca4330260d9dfed6565333735fa9
unknown <1184481334@qq.com>
2023-01-29 16:24:20 +0800

    Override Renderer

--- TinyEngine/src/Engine/Renderer/Renderer.cpp
+++ TinyEngine/src/Engine/Renderer/Renderer.cpp
@@ -6,34 +6,11 @@
 
 namespace Engine
 {
-	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData();
-
 	void Renderer::Init()
 	{
 		RenderCommand::Init();
 		Renderer2D::Init();
 	}
-
-	void Renderer::BeginScene(OrthographicCamera& camera)
-	{
-		m_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
-	}
-
-	void Renderer::EndScene()
-	{
-		
-	}
-
-	void Renderer::Submit(const Ref<VertexArray>& vertexArray, const std::shared_ptr<Shader>& shader, const glm::mat4& transform)
-	{
-		shader->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
-
-		vertexArray->Bind();
-		RenderCommand::DrawIndexed(vertexArray);
-	}
-
 
 	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
 	{
--- TinyEngine/src/Engine/Renderer/Renderer.h
+++ TinyEngine/src/Engine/Renderer/Renderer.h
@@ -2,9 +2,6 @@
 
 #include "Engine/Renderer/RenderCommand.h"
 
-#include "Engine/Renderer/OrthographicCamera.h"
-#include "Engine/Renderer/Shader.h"
-
 namespace Engine
 {
 	class Renderer
@@ -14,21 +11,9 @@ namespace Engine
 
 		static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 
-		static void BeginScene(OrthographicCamera& camera);
-		static void EndScene();
-		static void Submit(
-			const Ref<VertexArray>& vertexArray,
-			const Ref<Shader>& shader,
-			const glm::mat4& transform = glm::mat4(1.0f));
-
 		static void OnWindowResize(uint32_t width, uint32_t height);
 	
 	private:
-		struct SceneData
-		{
-			glm::mat4 ViewProjectionMatrix;
-		};
 
-		static SceneData* m_SceneData;
 	};
 }
